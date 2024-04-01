ca7fbbef45c7891dfe8096d823874b82640e3d88
fmway <fm18lv@gmail.com>
2024-04-01 12:06:07 +0700

    ah ah ah

--- bots/NamakuIsBot/main.ts
+++ bots/NamakuIsBot/main.ts
@@ -1,6 +1,6 @@
 import myBot from "$bots/NamakuIsBot/bot.ts"
 
-const TOKEN = Deno.env.get("DENO_TOKEN_NamakuIsBot");
+const TOKEN = Deno.env.get("TOKEN_NamakuIsBot");
 if (!TOKEN) throw new Error("Token Undefined");
 
 const bot = myBot(TOKEN);
--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
 import app from "$app";
 
-console.log(Deno.env.toObject());
 Deno.serve(app.fetch);
