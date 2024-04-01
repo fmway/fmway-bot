2d2e68e09d4ab22786660aa1648153a291be7a95
fmway <fm18lv@gmail.com>
2024-04-01 12:04:20 +0700

    jir lah tolol

--- app.ts
+++ app.ts
@@ -3,13 +3,13 @@ import { cors, logger, prettyJSON } from "hono/middleware";
 import { webhookCallback } from "grammy";
 import namakuIsBot from "$bots/NamakuIsBot/bot.ts";
 
-#const TOKEN_NamakuIsBot = Deno.env.get("DENO_TOKEN_NamakuIsBot");
-#if (!TOKEN_NamakuIsBot) throw new Error("Token NamakuIsBot not found!!!");
+//const TOKEN_NamakuIsBot = Deno.env.get("DENO_TOKEN_NamakuIsBot");
+//if (!TOKEN_NamakuIsBot) throw new Error("Token NamakuIsBot not found!!!");
 
 const app = new Hono({ strict: false });
 app.use("*", logger());
 app.use("*", prettyJSON());
 app.get("/", (c: Context) => c.text("Work jir"));
-#app.use("/NamakuIsBot", webhookCallback(namakuIsBot(TOKEN_NamakuIsBot), "hono")); 
+//app.use("/NamakuIsBot", webhookCallback(namakuIsBot(TOKEN_NamakuIsBot), "hono")); 
 
 export default app;
